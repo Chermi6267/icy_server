8ee644411214c09e08e43a260a8bd6af9bd025e6
Chermi6267 <chermi6267@gmail.com>
2024-10-15 21:38:19 +0900

    unpackImage

--- loadDataFromExcelToDb/utils/unpackImages.js
+++ loadDataFromExcelToDb/utils/unpackImages.js
@@ -1,42 +1,43 @@
 const fs = require("fs");
 const path = require("path");
 
-const defaultFolderPath = path.join(__dirname, "../../public/img/default");
-const smallFolderPath = path.join(__dirname, "../../public/img/small");
+const defaultFolderPath = path.join(__dirname, "../../img/default");
+const smallFolderPath = path.join(__dirname, "../../img/small");
 
-// fs.readdir(defaultFolderPath, (err, files) => {
-//   if (err) {
-//     console.error("Error reading directory 1:", err);
-//     return;
-//   }
+fs.readdir(defaultFolderPath, (err, files) => {
+  if (err) {
+    console.error("Error reading directory 1:", err);
+    return;
+  }
 
-//   files.forEach(async (file) => {
-//     const imgFolder = path.join(defaultFolderPath, file);
+  files.forEach(async (file) => {
+    const imgFolder = path.join(defaultFolderPath, file);
 
-//     fs.readdir(imgFolder, (err, files) => {
-//       if (err) {
-//         console.error("Error reading directory 2:", err);
-//         return;
-//       }
+    fs.readdir(imgFolder, (err, files) => {
+      if (err) {
+        console.error("Error reading directory 2:", err);
+        return;
+      }
 
-//       files.forEach((file) => {
-//         fs.rename(
-//           path.join(imgFolder, file),
-//           path.join(imgFolder, "../", file),
-//           (err) => {
-//             if (err) {
-//               console.log(err);
-//             } else {
-//               console.log(`Файл ${file} перемещен`);
-//             }
-//           }
-//         );
-//       });
-//     });
-//   });
-// });
+      files.forEach((file) => {
+        fs.rename(
+          path.join(imgFolder, file),
+          path.join(imgFolder, "../../../public/img/landmark/default", file),
+          (err) => {
+            if (err) {
+              console.log(err);
+            } else {
+              console.log(`Файл ${file} перемещен`);
+            }
+          }
+        );
+      });
+    });
+  });
+});
 
 fs.readdir(smallFolderPath, (err, files) => {
+  console.log(files);
   if (err) {
     console.error("Error reading directory 1:", err);
     return;
@@ -54,7 +55,7 @@ fs.readdir(smallFolderPath, (err, files) => {
       files.forEach((file) => {
         fs.rename(
           path.join(imgFolder, file),
-          path.join(imgFolder, "../", file),
+          path.join(imgFolder, "../../../public/img/landmark/small", file),
           (err) => {
             if (err) {
               console.log(err);
